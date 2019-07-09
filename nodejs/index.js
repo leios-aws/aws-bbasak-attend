61f8255d4e06e5ac5a7864ff397e390bbd286014
Minsoo Kim <leios@falinux.com>
2019-07-09 17:59:46 +0900

    빠삭 출석체크 구현

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,7 +5,7 @@ exports.handler = function(event, context, callback) {
     var authConfig = config.get('auth');
 
     var mainPage = {
-        uri: 'https://whooing.com/',
+        uri: 'http://bbasak.com/',
         method: 'GET',
         qs: {
         },
@@ -21,20 +21,18 @@ exports.handler = function(event, context, callback) {
         encoding: null
     };
     var loginPage = {
-        uri: 'https://whooing.com/auth/login',
+        uri: 'https://bbasak.com/bbs/login_check.php',
         method: 'POST',
         form: {
-            login: authConfig.id,
-            password: authConfig.pw,
-            submitting: '1',
-            go_to: ''
+            mb_id: authConfig.id,
+            mb_password: authConfig.pw
         },
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
             'Accept-Encoding': 'gzip, deflate',
             'Accept-Language': 'ko,en-US;q=0.8,en;q=0.6',
-            'Referer': 'https://whooing.com'
+            'Referer': 'http://bbasak.com/'
         },
         jar: true,
         gzip: true,
@@ -42,10 +40,9 @@ exports.handler = function(event, context, callback) {
         encoding: null
     };
     var attendPage = {
-        uri: 'https://whooing.com/',
-        method: 'POST',
-        form: {
-            section_id: 's'
+        uri: 'http://m.bbasak.com/rest/do_attendance.php',
+        method: 'GET',
+        qs: {
         },
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
@@ -62,13 +59,14 @@ exports.handler = function(event, context, callback) {
     request(mainPage).then(function(html){
         return request(loginPage);
     }).then(function(html) {
-        return request(attendPage);
-    }).then(function(html){
-        if (html.indexOf('top_user_logout') > -1) {
+        if (html.indexOf('/bbs/logout.php') > -1) {
             console.log("Login success");
+            return request(attendPage);
         } else {
             console.log(html.toString());
         }
+    }).then(function(html){
+        console.log(html.toString());
     }).catch(function(error) {
         if (error) {
             throw error;
